07693cd6a7bbbf10fcb810929579fd4203334039
zhanghanbo <1757889905@qq.com>
2020-03-28 20:12:39 +0800

    订单系统

---
+++ ZNZZOrder/src/main/java/com/znzz/order/OrderApplication.java
@@ -0,0 +1,13 @@
+package com.znzz.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApplication.class, args);
+	}
+
+}
